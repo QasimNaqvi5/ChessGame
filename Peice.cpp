eb21735c9cff104745b4d021520c32bba45106d1
qasim <bscs24113@itu.edu.pk>
2025-06-11 06:05:24 -0700

    Implemented Peice Logic

--- Peice.cpp
+++ Peice.cpp
@@ -1,1 +1,56 @@
-#pragma once
+#include "Peice.h"
+#include<iostream>
+using namespace std;
+Piece::Piece(int px, int py, PColor _c, Board* b, int c, int r, Texture2D tex) :P{ px,py }, C{ _c }, B{ b }
+{
+
+    col = c;
+    row = r;
+    texture = tex;
+}
+void Piece::Draw(int squaresize)
+{
+    if (texture.id == 0) {
+        cout << "Texture not loaded for piece at " << row << ", " << col << endl;
+        return;
+    }
+    //DrawTexture(texture, col * squaresize, row * squaresize,WHITE);
+    DrawTextureEx(texture,
+        Vector2{ (float)(P.y * squaresize), (float)(P.x * squaresize) },
+        0.0f,
+        (float)squaresize / texture.width,
+        WHITE);
+}
+void Piece::move(Position Des)
+{
+    this->P = Des;
+
+}
+
+PColor Piece::getClr()
+{
+    return C;
+}
+bool Piece::IsVertical(Position D)
+{
+    return P.y == D.y;
+}
+bool Piece::IsHorizontal(Position D)
+{
+    return P.x == D.x;
+}
+bool Piece::IsDiagonal(Position D)
+{
+    int dx = abs(P.x - D.x);
+    int dy = abs(P.y - D.y);
+    return dx == dy;
+
+}
+
+
+
+
+
+
+
+
--- Peice.h
+++ Peice.h
@@ -1,5 +1,38 @@
 #ifndef PEICE_H
 #define PEICE_H
 
+#include<iostream>
+#include"Position.h"
+#include<raylib.h>
+class Board;
+class Piece
+{
+protected:
+	Position P;
+	PColor C;
+	Board* B;
+	int col, row;
+	Texture2D texture;
+
+
+
+
+
+public:
+	Piece(int px, int py, PColor _c, Board* b, int c, int r, Texture2D tex);
+	void move(Position Des);
+	//virtual void Draw() = 0;
+	void Draw(int squareSize);
+	virtual bool IsLegalMove(Position D) = 0;
+	PColor getClr();
+	bool IsVertical(Position D);
+	bool IsHorizontal(Position D);
+	bool IsDiagonal(Position D);
+	
+
+};
+
+
 
 #endif 
+
